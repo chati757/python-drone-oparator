a6865d7423e228e79f9ebabec70b095e37c5bc22
bch_chati757 <chati757@users.noreply.github.com>
2017-03-12 21:04:50 +0700

    update 2104120317

--- station_position.py
+++ station_position.py
@@ -1,24 +1,31 @@
 from unicurses import * 
 
+#-----------------------------------------initalize stage----------------------------------------------
+stdscr = initscr() #start up screen and initalize ncurses
+#-----------determate default window---------------
+start_color()#default is white
+noecho()#not show txt when typing
+curs_set(False)#not show cursor
+keypad(stdscr,True)#register arrow key 
+
+#-------------draft window------------------
+#draftwin(stdscr)
+#center(79 25) all(158 50) work space (154 44)
+#-------------------------------------------
+
+#-----------------layout--------------------
+MAX_Y,MAX_X=getmaxyx(stdscr)
+
+#----------position panel layout------------
+#size high 22 width 80 
+#position x = 154-(154-80) = 74
+#position y = 44/2 = 22
+POSITION_PANEL_Y=22
+POSITION_PANEL_X=74
+
 def main():
-    
-    stdscr = initscr() #start up screen and initalize ncurses
-
-    #-----------determate window---------------
-    start_color()#default is white
-    noecho()#not show txt when typing
-    curs_set(False)#not show cursor
-    keypad(stdscr,True)#register arrow key 
-
-    #-------------draft window------------------
-    #draftwin(stdscr)
-    #center(79 25) all(158 50) work space (154 44)
-    #position panel(80 22)
-    #-------------------------------------------
-
-    max_y,max_x=getmaxyx(stdscr)
     #-----------create position panel-----------
-    create_slot((max_y-28),(max_x-84))
+    create_position_panel()
 
     running = True
     while(running):
@@ -32,13 +39,18 @@ def main():
     return 0
 
 def draftwin(stdscr):
-    max_y,max_x=getmaxyx(stdscr)
-    move(max_y/2,max_x/2)
-    addstr("[ @ ]"+"center x= "+str(max_x/2)+" center y= "+str(max_y/2))
-    move((max_y/2)+1,(max_x/2))
-    addstr("[max colums x= "+str(max_x)+" max rows y= "+str(max_y)+"]")
+    global MAX_Y
+    global MAX_X
+    move(MAX_Y/2,MAX_X/2)
+    addstr("[ @ ]"+"center x= "+str(MAX_X/2)+" center y= "+str(MAX_Y/2))
+    move((MAX_Y/2)+1,(MAX_X/2))
+    addstr("[max colums x= "+str(MAX_X)+" max rows y= "+str(MAX_Y)+"]")
 
-def create_slot(y_axis,x_axis):
+def create_position_panel():
+    global POSITION_PANEL_Y
+    global POSITION_PANEL_X
+    global MAX_Y
+    global MAX_X
     #-------------station slot panel-----------
     #total width 80 high 22
     window_slot=newwin(22,80,0,0)
@@ -48,6 +60,8 @@ def create_slot(y_axis,x_axis):
 
     #-------------station slot node------------
     #total width 20  high 22
+    #add condition not less 20 and 22
+
     window_sub_slot_1=newwin(20,20,0,0)
     box(window_sub_slot_1)
     wmove(window_sub_slot_1,1,2)
@@ -69,10 +83,12 @@ def create_slot(y_axis,x_axis):
     #-------------all panel component----------
     ws_panel=new_panel(window_slot)
     wss_panel_1=new_panel(window_sub_slot_1)
+
     #wss_panel_2=new_panel(window_sub_slot_2)
 
     #-------------manage panel position--------
     move_panel(wss_panel_1,1,0)
+
     #move_panel(wss_panel_2,1,20)
 
     #-------------panel update stage-----------
